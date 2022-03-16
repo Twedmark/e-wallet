ff180b7e346419adc54063dd9abb2b825c49fc55
Twedmark <axel.Twedmark@hotmail.com>
2022-03-16 13:15:16 +0100

    all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import Home from './views/Home'
+import AddCard from './views/AddCard'
 
 function App() {
+
+  const [ cards, uppdateCards ] = useState([]);
+
+
+  function addCard(card){
+    uppdateCards((oldCards) =>{
+      oldCards.push(card)
+      return oldCards;
+    });
+  }
+
+  function removeCard(index){
+    let removeCards = cards;
+    removeCards.splice(index, 1);
+    uppdateCards(removeCards)
+  }
+
+
+  // useEffect(() => {
+  //   const saved = JSON.parse(localStorage.getItem("cards")
+  //
+  //     uppdateCards(JSON.parse(localStorage.getItem("cards")));
+  //
+  // }, []);
+  //
+  // useEffect(() => {
+  //   localStorage.clear();
+  //   localStorage.setItem("cards", JSON.stringify(cards));
+  // }, [cards]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route path="/" element={ <Home cards={ cards } removeCard={ removeCard }/> } />
+      <Route path="/AddCard" element={ <AddCard addCard={ addCard }/> } />
+    </Routes>
     </div>
   );
 }
